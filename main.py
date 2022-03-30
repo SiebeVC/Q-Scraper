e3aec8e1eddbe7029847b51d4c873284b2cd4457
Siebe Van Campenhout <vcsiebe02@gmail.com>
2022-03-30 20:13:44 +0200

    Volledig automatisch + description update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import datetime
 from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
@@ -53,12 +54,19 @@ def clear_playlist():
     # remove all songs
     spotify.playlist_remove_all_occurrences_of_items(playlist_id, track_ids)
 
+
+def search_track(title, artist):
+    result = spotify.search(q=f'{title} {artist}')
+    return result['tracks']['items'][0]['id']
+
+
 def add_songs_to_playlist(top40):
     def get_trackid_from_spoturl(track):
         if 'spotify_url' in track.keys():
             if track['spotify_url'].split('/')[-1] is not None:
                 return track['spotify_url'].split('/')[-1][0:22]
         print(f'{top40.index(track) + 1}. ' + track['title'] + ' - ' + track['artist']['name'])
+        return search_track(track['title'], track['artist']['name'])
 
 
     track_ids = list(map(get_trackid_from_spoturl, top40))
@@ -66,8 +74,14 @@ def add_songs_to_playlist(top40):
     spotify.playlist_add_items(playlist_id=playlist_id, items=track_ids)
 
 
+def change_description():
+    now = datetime.datetime.now()
+    spotify.playlist_change_details(playlist_id=playlist_id, description=f'De beste hits van het moment in België met wekelijkse updates.  (laatste update: {now.strftime("%m/%d/%Y %H:%M:%S")})')
+
+
 if __name__ == '__main__':
     latest = get_latest_editions()
     top40 = get_top40(latest)
     clear_playlist()
     add_songs_to_playlist(top40)
+    change_description()
