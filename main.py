affca0bccf4644e5157de466b2b695127e90a534
Siebe Van Campenhout <vcsiebe02@gmail.com>
2022-03-30 17:52:40 +0200

    part 1 done, getting top 40

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print('Q-scraper')
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+url_get_editions = os.getenv('url_get_editions')
+url_get_top40 = os.getenv('url_get_top40')
+
+try:
+    editons = requests.get(url_get_editions).json()
+    last_edition = editons['editions'][0]
+except Exception as e:
+    print(e)
+    print('ERROR getting edition Q-Top 40')
+
+try:
+    response = requests.get(url_get_top40.format(last_edition["id"]))
+    top40 = response.json()['tracks']
+
+    for i in top40:
+        print(i['title'])
+except Exception as e:
+    print(e)
+    print(f'ERROR getting last edition with id {last_edition["id"]} op datum {last_edition["name"]}')
